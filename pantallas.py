b05752e54e47fec07824c9c780d6871dc9116de1
Analia Lujan Zunino <analiazunino@gmail.com>
2015-05-16 23:27:53 -0300

    Se crearon label y caja de texto para la coordenadas

--- pantallas.py
+++ pantallas.py
@@ -57,3 +57,27 @@ class PantallaJuego(pilasengine.escenas.Escena):
         self.protagonista.escala = 0.5
         self.protagonista.x = col_a_x(2567)
         self.protagonista.y = fil_a_y(392)
+
+        self.label_fila = self.pilas.actores.Texto("Fila :")
+        self.label_fila.centro = ("izquierda", "centro")
+        self.label_fila.x = col_a_x(60)
+        self.label_fila.y = fil_a_y(1150)
+        self.label_fila.color = pilasengine.colores.negro
+
+        self.input_fila = self.pilas.interfaz.IngresoDeTexto("")
+        self.input_fila.centro = ("izquierda", "centro")
+        self.input_fila.x = col_a_x(120)
+        self.input_fila.y = fil_a_y(1150)
+        self.input_fila.escala = 2
+
+        self.label_columna = self.pilas.actores.Texto("Columna :")
+        self.label_columna.centro = ("izquierda", "centro")
+        self.label_columna.x = col_a_x(60)
+        self.label_columna.y = fil_a_y(1250)
+        self.label_columna.color = pilasengine.colores.negro
+
+        self.input_columna = self.pilas.interfaz.IngresoDeTexto("")
+        self.input_columna.centro = ("izquierda", "centro")
+        self.input_columna.x = col_a_x(120)
+        self.input_columna.y = fil_a_y(1250)
+        self.input_columna.escala = 2
